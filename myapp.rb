b536e5a60d24250499e6ab6465c6c96ad17d9562
tararico <emipu.uq@gmail.com>
2018-09-05 21:22:06 +0900

    fix indent and delete variable

--- myapp.rb
+++ myapp.rb
@@ -4,11 +4,9 @@ require 'pathname'
 require 'date'
 
 get '/memos' do
-  @title = 'memoapp'
-  @content = 'メモアプリ'
   @ichirans = Pathname.glob("memos/*.txt").map{|i| i.basename(".txt")}
   erb :index
-end    
+end
 
 post '/memos' do
   File.open("memos/#{params[:memo_title]}.txt", "w") do |f| 
@@ -18,32 +16,26 @@ post '/memos' do
 end
     
 get '/memos/new' do
-  @title = 'memoapp'
-  @content = 'メモアプリ'
   erb :new
 end
 
 get '/memos/:memo_title/edit' do
-  @title = 'memoapp'
-  @content = 'メモアプリ'
   @text = File.read("memos/#{params[:memo_title]}.txt")
   @memo_title = "#{params[:memo_title]}"
   erb :edit
 end
 
 get '/memos/:memo_title' do
-  @title = 'memoapp'
-  @content = 'メモアプリ'
   @text = File.read("memos/#{params[:memo_title]}.txt")
   @memo_title = "#{params[:memo_title]}"
   erb :show
 end
 
 patch '/memos/:memo_title' do
-    File.open("memos/#{params[:memo_title]}.txt", "w") do |f| 
-      f.puts(params[:content])
-    end
-    redirect '/memos'
+  File.open("memos/#{params[:memo_title]}.txt", "w") do |f|
+    f.puts(params[:content])
+  end
+  redirect '/memos'
 end
 
 delete '/memos/:memo_title' do
