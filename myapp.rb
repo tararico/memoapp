4b1a107313a84d6ef188ac1626e7b31380f95aeb
tararico <emipu.uq@gmail.com>
2018-09-24 21:53:49 +0900

    fix many things

--- myapp.rb
+++ myapp.rb
@@ -4,12 +4,16 @@ require 'pathname'
 require 'yaml'
 
 get '/memos' do
-  @memos = Pathname.glob("memos/*.txt").map{|i| i.basename(".txt")}
+  @file_names = Pathname.glob("memos/*.txt").map{|i| i.basename(".txt").to_s}
+  @memos = {}
+  @file_names.each do |i|
+   @memos[i] =  YAML.load_file("memos/#{i}.txt")["title"]
+  end
   erb :index
 end
 
 post '/memos' do
-  num = Pathname.glob("memos/*.txt").map{|i| i.basename(".txt").to_s.to_i}.last || 0
+  num = Pathname.glob("memos/*.txt").map{|i| i.basename(".txt").to_s.to_i}.max || 0
   file_name = num + 1
   data = {'title'=> params[:memo_title], 'body'=> params[:content]}
   File.open("memos/#{file_name}.txt", "w") do |f|
@@ -26,19 +30,22 @@ get '/memos/:memo_title/edit' do
   data = YAML.load_file("memos/#{params[:memo_title]}.txt")
   @text = data['body'] 
   @memo_title = data['title'] 
+  @file_name = params[:memo_title]
   erb :edit
 end
 
 get '/memos/:memo_title' do
+  @file_name = params[:memo_title]
   data = YAML.load_file("memos/#{params[:memo_title]}.txt")
-  @text = data['body'] 
-  @memo_title = data['title'] 
+  @text = data['body']
+  @memo_title = data['title']
   erb :show
 end
 
 patch '/memos/:memo_title' do
+  data = {'title'=> params[:title], 'body'=> params[:content]}
   File.open("memos/#{params[:memo_title]}.txt", "w") do |f|
-    f.puts(params[:content])
+    YAML.dump data, f
   end
   redirect '/memos'
 end
